a0960915c170297f5f76c303a7844f7ca98a1c83
Kajalkoli <45035266+KajalKoli@users.noreply.github.com>
2021-02-24 22:46:21 +0530

    Adding Author and Book model

---
+++ BookArchive/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookArchive.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public String name { get; set; }
+        public string email { get; set; }
+        public Int64 phone { get; set; }
+    }
+}
---
+++ BookArchive/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BookArchive.Models;
+
+namespace BookArchive.Models
+{
+    public class Book
+    {
+        //Author author = new Author();
+
+        public string ISBN { get; set; }
+        public string name { get; set; }
+        public string authorId { get; set; }
+        public HttpPostedFile frontCover { get; set; }
+        public HttpPostedFile backCover { get; set; }
+        public float price { get; set; }
+        public int yearPublish { get; set; }
+        public string edition { get; set; }
+        public String Publisher { get; set; }
+        public HttpPostedFile bookFile { get; set; }
+
+    }
+}
